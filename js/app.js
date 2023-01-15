48fe6b5f02255bc93702daf7512c3530c05411d4
Allan Simon <allan.simon@supinfo.com>
2023-01-15 16:29:57 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
 (function() {
 "use strict";
+
+// we can't directly use 'fetch' because it does not offer
+// the responseXML property
+const makeXMLRequest = function (url) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.responseXML);
+            } else {
+                reject({status: xhr.status, statusText: xhr.statusText});
+            }
+        };
+        xhr.onerror = function () {
+            reject({status: xhr.status, statusText: xhr.statusText});
+        };
+        xhr.send();
+    });
+}
+   
   
-var xml =  new window.DOMParser().parseFromString(
-  await (await fetch('data/cfdict.xml')).text()
-);
+var xml =  await makeXMLRequest('data/cfdict.xml');
 console.log("done loading chinese data");
-  
 console.log(xml);
-  
-  
-  
+ 
 })();
