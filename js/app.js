5b3fb6169e707a76c6078af9e0a981add90000dc
Allan Simon <allan.simon@supinfo.com>
2023-01-15 16:01:20 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-
+(function() {
+"use strict";
+  
+var xml =  new window.DOMParser().parseFromString(
+  await (await fetch('data/cfdict.xml')).text()
+);
+console.log("done loading chinese data");
+  
+console.log(xml);
+  
+  
+  
+})();
